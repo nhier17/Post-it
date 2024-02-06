cefd477e9c9b39a1339f8fb7a83561572d156d45
Abraham <abrahamnhier97@gmail.com>
2024-02-06 15:56:50 +0300

    dashboard setup

---
+++ pages/api/posts/authPosts.tsx
@@ -0,0 +1,38 @@
+import prisma from "../../../prisma/client"
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "../auth/[...nextauth]"
+import { NextApiRequest, NextApiResponse } from "next"
+
+
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+ if (req.method === "GET" ) {
+const session = await getServerSession(req,res, authOptions)
+if(!session) return res.status(401).json({msg: "Please sign in "})
+//get auth user post
+try {
+const data = await prisma.user.findUnique({
+    where: {
+        email: session.user?.email,
+    },
+    include: {
+        posts: {
+            orderBy: {
+                createdAt: "desc"
+            },
+            include: {
+                Comment: true
+            }
+        }
+    }
+})
+res.status(200).json({data})
+} catch (error) {
+res.status(403).json({ error: "Error has occured whilst making a post"})
+}
+
+ }
+}
